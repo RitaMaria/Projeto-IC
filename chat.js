3d89f14fb3af30727f8e4ca1df55008c066ec393
RitaMaria <67376599+RitaMaria@users.noreply.github.com>
2021-11-15 20:25:22 +0000

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,35 @@
+function enviar_msg() {
+    let userInput = document.querySelector("input").value;
+
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <i id="img_user" class="fa fa-user-o"></i>
+    </div>`;
+
+    document.querySelector(".chat-area").insertAdjacentHTML("beforeend", temp);
+    document.querySelector("input").value = '';
+}
+
+function open_attach_file_popup() {
+    document.getElementById("attach_file_popup").style.display = "block";
+}
+
+function close_attach_file_popup() {
+    document.getElementById("attach_file_popup").style.display = "none";
+}
+
+function sendpdf() {
+    let p = `<div class="out-msg">
+    <span class="my-msg"><a href="ranking@13Set2021.pdf">ranking@13Set2021.pdf</a></span>
+    <i id="img_user" class="fa fa-user-o"></i>
+    </div>`;
+
+    document.querySelector(".chat-area").insertAdjacentHTML("beforeend", p);
+
+    document.getElementById("attach_file_popup").style.display = "none";
+
+}
+
+function iniciar_chamada() {
+    location.href="chamada.html";
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+function voltar_chat() {
+    location.href = "chat.html";
+}
+
+function iniciar_chat() {
+    location.href = "chat.html";
+}
+
+function iniciar_chamada() {
+    location.href = "chamada.html";
+}
